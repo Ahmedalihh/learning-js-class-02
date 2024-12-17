bc61cf8a73dd060e0ba42172ea9e045b9b0f82c4
Ahmedalihh <ahmedalibbb227@gmail.com>
2024-12-17 23:06:56 +0500

    learning js

---
+++ app.js
@@ -0,0 +1,39 @@
+// alert("hello class");
+
+// variables
+// var , let , const
+// var username = "ahmed ali";
+
+// alert(username);
+
+// var num1 = 10
+// alert(num1)
+
+// javascript data types
+// 1. string => group of character => (ahmed@123) =>"ali" / 'ali'
+// 2. number => 10 , 40.40
+// 3. boolean
+// 4. null
+// 5. undefined
+
+// var value = "SMIT"; //string data type
+// var value = "SMIT"; //string data type
+
+// var num1 = 100; //number data type
+// var num1 = 100.5; //number data type
+
+// var value = "SMIT"
+
+// var value = "SAYLANI"
+// console.log(value)
+
+// var myname => decleration
+// "ahmed ali" => initlization
+// myname ="jaffer" reinitilzation
+
+// variables don't use number or special character except ($ or _)
+// smitValue camelcase
+
+// var result;
+// result = "a="
+// console.log(result)
